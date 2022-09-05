121f00dfa2c72962d3a75c185b38d43663ba8692
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-09-05 19:53:14 +0530

    Err?

---
+++ src/api/app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+import subprocess
+app = Flask(__name__)
+
+
+  
+@app.route('/')
+def index():
+    return render_template("index.html")
+   
+
+   
+
+@app.route('/ex/<string:c>/<string:p>')
+def main(c,p):
+    result = subprocess.run([c,p], stdout=subprocess.PIPE)
+    lines = result.stdout.splitlines()
+ 
+    op = ' '.join([str(elem) for elem in lines])
+    return op
+    
+@app.route('/ex2/<string:c>')
+def main2(c):
+    result2 = subprocess.run([c], stdout=subprocess.PIPE)
+    lines2 = result2.stdout.splitlines()
+ 
+    op2 = ' '.join([str(elem) for elem in lines2])
+    return op2
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+    
+  
+
