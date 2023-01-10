441607d84e89993b534010d573a747f600200d6a
Shour <shourjjomajum@outlook.com>
2023-01-10 19:04:09 +0530

    Update app.py

--- src/api/app.py
+++ src/api/app.py
@@ -16,15 +16,15 @@ def main(c,p):
     result = subprocess.run([c,p], stdout=subprocess.PIPE)
     lines = result.stdout.splitlines()
  
-    op = ' '.join([str(elem) for elem in lines])
+    op = ' '.join([elem.decode() for elem in lines])
+
     return op
     
 @app.route('/ex2/<string:c>')
 def main2(c):
     result2 = subprocess.run([c], stdout=subprocess.PIPE)
     lines2 = result2.stdout.splitlines()
- 
-    op2 = ' '.join([str(elem) for elem in lines2])
+    op2 = ' '.join([elem.decode() for elem in lines2])
     return op2
     
 
