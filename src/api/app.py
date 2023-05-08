395256f9576eacfa8d0e5c19fd6f8b6433436c7c
Shourjjo Majumder <shourjjomajum@outlook.com>
2023-05-08 11:21:06 +0530

    now only ex exists which can run both types of command

--- src/api/app.py
+++ src/api/app.py
@@ -13,32 +13,23 @@ def index():
     </head>
     <body>
        <h1> Dashboard </h1>
-        <p>One argument</p> <br>
+        <p>Run command</p> <br>
         <p> {host}/ex/{command}/{arg} </p>
-       <p> No arguments </p>  <br>
-      <p> {host}/ex2/{command} </p>
+      
     </body>
 </html>
 '''
 
    )
 
-@app.route('/ex/<string:c>/<string:p>')
-def main(c,p):
-    result = subprocess.run([c,p], stdout=subprocess.PIPE)
+@app.route('/ex/<path:args>')
+def main(args):
+    cmd = args.split('/')
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
     lines = result.stdout.splitlines()
- 
     op = ' '.join([elem.decode() for elem in lines])
-
     return op
-    
-@app.route('/ex2/<string:c>')
-def main2(c):
-    result2 = subprocess.run([c], stdout=subprocess.PIPE)
-    lines2 = result2.stdout.splitlines()
-    op2 = ' '.join([elem.decode() for elem in lines2])
-    return op2
-    
+
 
 
 if __name__ == "__main__":
